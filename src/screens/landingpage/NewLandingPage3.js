d6fa3eec1cc2c0fdf0dea5a6e54f45af81e2a1ae
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-02-25 22:04:10 +0700

    updated cv

---
+++ src/screens/landingpage/NewLandingPage3.js
@@ -0,0 +1,308 @@
+import React, { Component } from 'react';
+import './NewLandingPage2.css';
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, Element , Events, animateScroll as scroll, scroller } from 'react-scroll';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import {FaPencilAlt, FaFacebook, FaLinkedin, FaInstagram, FaGithub, FaWhatsapp} from 'react-icons/fa';
+import Collapse from 'rc-collapse';
+import HamburgerMenu from 'react-hamburger-menu';
+import { push as Menu } from 'react-burger-menu';
+// import { useSwipeable, Swipeable } from 'react-swipeable'
+import Moment from 'moment-timezone';
+var Panel = Collapse.Panel;
+require('rc-collapse/assets/index.css');
+class NewLandingPage extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: Moment().format('YYYY'),
+            menu: [
+                {name: 'About', stts: 'active'},
+                {name: 'Experience', stts: ''},
+                {name: 'Education', stts: ''},
+                {name: 'Skills', stts: ''},
+                {name: 'Contact', stts: ''}
+            ],
+            open: false,
+            touchIdentifier: false
+        };
+        this.scrollToTop = this.scrollToTop();
+    }
+
+    componentWillUnmount() {
+        Events.scrollEvent.remove('begin');
+        Events.scrollEvent.remove('end');
+    }
+
+    scrollToTop = () => {
+        scroll.scrollToTop();
+    }
+
+    scrollTo = (arg) => {
+        scroller.scrollTo(arg, {
+          duration: 800,
+          delay: 0,
+          smooth: 'easeInOutQuart'
+        })
+        this.handleClickMenu(arg);
+    }
+
+    scrollToWithContainer = () => {
+
+        let goToContainer = new Promise((resolve, reject) => {
+    
+          Events.scrollEvent.register('end', () => {
+            resolve();
+            Events.scrollEvent.remove('end');
+          });
+    
+          scroller.scrollTo('scroll-container', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+          });
+    
+        });
+    
+        goToContainer.then(() =>
+          scroller.scrollTo('scroll-container-second-element', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart',
+            containerId: 'scroll-container'
+          }));
+    }
+
+    handleClickMenu = (arg) => { 
+        const menu= [
+            {name: 'About Me', stts: ''},
+            {name: 'Experience', stts: ''},
+            {name: 'Education', stts: ''},
+            {name: 'Skills', stts: ''},
+            {name: 'Contact', stts: ''}
+        ];
+        const indexMenuSelected = menu.findIndex( (element) => element.name === arg);
+        menu[indexMenuSelected] = 'Active'
+
+        this.setState({menu,open: !this.state.open});
+    }
+
+    handleClick = () => {
+        this.setState({
+            open: !this.state.open
+        });
+        console.log(this.state.date)
+    }
+    overlayClicked = () => {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+
+    downloadCV = () => {
+        const element = document.createElement("a");
+        element.href = 'Yusuf-Eko-Anggoro-ATS-Resume 2025.pdf'
+        element.target = '_blank';
+        element.download = ''
+
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+        element.parentNode.removeChild(element);
+    }
+    
+
+    render(){
+        const closeAllMenusOnEsc = (e) => {
+            e = e || window.event;
+            if (e.key === 'Escape' || e.keyCode === 27) {
+              this.setState({open: false});
+            }
+        };
+        return (
+            <div id="outer-container">
+                <Menu disableOverlayClick={() => this.overlayClicked()} customOnKeyDown={closeAllMenusOnEsc} burgerButtonClassName={ "button-ganggu" } overlayClassName={'overlay-ganggu'} className={ "my-menu" }  isOpen={this.state.open} pageWrapId={ 'page-wrap' } outerContainerId={ "outer-container" }>
+                    <aside>
+                        <div className='text-center'>
+                            <div className='author-img'></div>
+                            <h1 className='full-name'>
+                                <a>Yusuf Eko Anggoro</a>
+                            </h1>
+                            <span className="position"><a href="">Backend Dev</a></span>
+                        </div>
+                        <div className='nav'>
+                            <ul>
+                                {
+                                    this.state.menu.map((object, index) => {
+                                        return(
+                                            <li>
+                                                <Link onClick={() => this.handleClick()} className="anchor" to={object.name} spy={true} smooth={true} duration={500}>{object.name}</Link>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                        <div className='footer'>
+                        <p>
+                            <small>
+                                &copy;Copyright {this.state.date} All rights reserved | This web is made with <FontAwesomeIcon icon={faHeart} /> by <a>Yusuf</a>
+                            </small>
+                        </p>
+                    </div>
+                    </aside>
+                </Menu>
+                <div className='my-container' id="page-wrap">
+                    <aside>
+                        <div className='text-center'>
+                            <div className='author-img'></div>
+                            <h1 className='full-name'>
+                                <a>Yusuf Eko Anggoro</a>
+                            </h1>
+                            <span className="position"><a href="">Backend Dev</a></span>
+                        </div>
+                        <div className='nav'>
+                            <ul>
+                                {
+                                    this.state.menu.map((object, index) => {
+                                        return(
+                                            <li>
+                                                <Link  className="anchor" to={object.name} spy={true} smooth={true} duration={500}>{object.name}</Link>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                        <div className='footer'>
+                            <p>
+                                <small>
+                                    &copy;Copyright {this.state.date} All rights reserved | This web is made with <FontAwesomeIcon icon={faHeart} /> by <a>Yusuf</a>
+                                </small>
+                            </p>
+                        </div>
+                    </aside>  
+                    <main>
+                        <div className='burger'>
+                            <HamburgerMenu
+                                isOpen={this.state.open}
+                                menuClicked={() => this.handleClick()}
+                                width={18}
+                                height={15}
+                                strokeWidth={1}
+                                rotate={0}
+                                color='black'
+                                borderRadius={0}
+                                animationDuration={0.5}
+                            />
+                        </div>
+                        <Element name="About" className="about-wrap">
+                            <h2 className="heading-meta">About Me</h2>
+                            <div className='desc'>
+                                <p style={{textAlign: 'justify', textJustify: 'inter-word'}}>
+                                    <strong>Hi, I'm Yusuf Eko Anggoro</strong>, a <strong>Back-End Developer</strong> with <strong>5+ years of experience</strong> in developing, and maintaining scalable systems. I have a <strong>strong passion for coding</strong> and a deep enthusiasm for <strong>continuous learning</strong>, especially in the <strong>latest technologies</strong>. My <strong>expertise</strong> includes <strong>Node.js, TypeScript, and Golang</strong> for back-end development in a <strong>microservices architecture</strong>.
+                                </p>
+                            </div>
+                            <div>
+                            <button onClick={ () => this.downloadCV()}>Download CV</button>
+                            </div>
+                        </Element>
+                        <Element name="Experience" className="experience-wrap">
+                            <h2 className="heading-meta">Experience</h2>
+                            <VerticalTimeline lineColor="#f2f3f7" layout="1-column-left">
+                            <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                                    date="August 2019 - 2025"
+                                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    icon={<FaPencilAlt/>}
+                                >
+                                    <h5 className="vertical-timeline-element-title">Back-End Developer at Telkom Indonesia</h5>
+                                    <p>
+                                        <ul className="ul-vertical-timeline-content">
+                                            <li>Developed and maintained backend services using Node.js, TypeScript, and Golang within a microservices architecture.</li>
+                                            <li>Designed and optimized databases (MongoDB, PostgreSQL, Oracle, MySQL, Redis) for high performance and scalability.</li>
+                                            <li>Implemented cloud storage solutions with Minio, AWS S3, and integrated Firebase for push notifications.</li>
+                                            <li>Ensured code quality and stability through automated testing with Mocha, Chai, and Jest.</li>
+                                            <li>Used Kafka for real-time communication between services.</li>
+                                            <li>Used Elasticsearch to improve data search.</li>
+                                            <li>Used the ELK Stack (Elasticsearch, Logstash, Kibana) to monitor and analyze logs, based on configurations set by the DevOps team.</li>
+                                            <li>Used Jenkins for CI/CD processes that were set up by the DevOps team.</li>
+                                            <li>Implemented API security best practices, including Basic Authentication, JWT, Rate Limiting, and CORS</li>
+                                            <li>Implemented encryption and decryption to protect sensitive data.</li>
+                                            <li>Collaborated within Agile development teams, actively participating in sprint planning, daily stand-ups, and retrospectives to drive continuous improvement and efficient project execution.</li>
+                                            <li>Used Microservices and Domain-Driven Design (DDD) principles to build flexible and scalable systems.</li>
+                                            <li>Used Docker for containerization and Git for version control and collaboration.</li>
+                                        </ul>
+                                    </p>
+                            </VerticalTimelineElement>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                                    date="July 2019 - August 2019"
+                                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    icon={<FaPencilAlt/>}
+                                >
+                                    <h5 className="vertical-timeline-element-title">Graduated as Full Stack Developer at Arkademy</h5>
+                                    <p className="p-vertical-timeline-content">Arkademy is a software engineering program that provides a 6 weeks hands-on coding curriculum that focuses on mobile development with react native and node JS.</p>
+                                </VerticalTimelineElement>
+                            </VerticalTimeline>
+                        </Element>
+                        <Element name="Education" className="education-wrap">
+                            <h2 className="heading-meta">Education</h2>
+                            <Collapse defaultActiveKey="0" style={{marginBottom: '7px'}} accordion={true}>
+                                <Panel className="panel-education-content" header="Indraprasta PGRI University (2020-2024)" headerClass="my-header-class">
+                                    <p>Completed a final project research on the Decision Support System for Employee Recruitment at PT Telkom, implementing the AHP method to enhance objectivity and efficiency in the selection process, using Java (NetBeans) and MySQL as supporting technologies.</p>
+                                    <p><strong>GPA: 3.5</strong></p>
+                                </Panel>
+                            </Collapse>
+                            <Collapse defaultActiveKey="0" accordion={true}>
+                                <Panel className="panel-education-content" header="Vocational High School 24 Jakarta (2017-2019)" headerClass="my-header-class">
+                                I studied and explored programming, software development, and database management. Proficient in algorithms, data structures, and developing web and desktop applications, with experience in software development projects both individually and in teams.
+                                </Panel>
+                            </Collapse>
+                        </Element>
+                        <Element name="Skills" className="skills-wrap">
+                            <h2 className="heading-meta">My Skills</h2>
+                            <Collapse defaultActiveKey="0" accordion={true} style={{marginBottom: '7px'}}>
+                                <Panel header="Back-End" headerClass="my-header-class backend">
+                                    <p>
+                                        <ul className='ul-skills-content'>
+                                            <li>Programming Languages: JavaScript (Node.js), TypeScript, GO (Golang), Java, Laravel</li>
+                                            <li>Databases: MongoDB, Oracle, PostgreSQL, MySQL, Redis</li>
+                                            <li>Cloud & Storage: Minio, AWS S3, Firebase (Push Notification)</li>
+                                            <li>Testing: Mocha, Chai, Jest</li>
+                                            <li>Message Broker: Kafka</li>
+                                            <li>Search & Indexing: Elasticsearch</li>
+                                            <li>Monitoring & Logging: ELK Stack (Elasticsearch, Logstash, Kibana), Jenkins</li>
+                                            <li>API Development: REST API</li>
+                                            <li>Authentication & Security: Basic Authentication, JWT (JSON Web Token), Rate Limiting, CORS, Encryption & Decryption</li>
+                                            <li>Methodologies: Agile</li>
+                                            <li>Software Design & Architecture: Microservices, Domain-Driven Design (DDD)</li>
+                                            <li>Tools: Docker, Git</li>
+                                        </ul>
+                                    </p>
+                                </Panel>
+                            </Collapse>
+                        </Element>
+                        <Element name="Contact" className="contact-wrap">
+                            <div className='contact'>
+                                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/yousufe24/"><FaLinkedin className='linkedin' color='#0077B5' size={35} /></a>
+                            </div>
+                            <div className='contact'>
+                                <a target="_blank" rel="noopener noreferrer" href="https://github.com/yusufekoanggoro"><FaGithub color="black" size={35} /></a>
+                            </div>
+                            <div className='contact'>
+                                <a target="_blank" rel="noopener noreferrer" href="https://wa.me/6285934531182"><FaWhatsapp size={35}  color='#25d366'/></a>
+                            </div>
+                        </Element>
+                    </main>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewLandingPage;
