565e2332a0d6cda634971367c5f26baeda02887f
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-04-08 20:39:53 +0700

    update

---
+++ precache-manifest.318e81834144225a42c4810f6d77b233.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6eac6ea74d6a89106797af766a04ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef6ea22d5bf7196f602e",
+    "url": "./static/css/2.bb848794.chunk.css"
+  },
+  {
+    "revision": "4b2b2e77eebff26e1c56",
+    "url": "./static/css/main.99955bce.chunk.css"
+  },
+  {
+    "revision": "ef6ea22d5bf7196f602e",
+    "url": "./static/js/2.b53398e9.chunk.js"
+  },
+  {
+    "revision": "7824ad0ee5efcf3a12102aa349ea3f9d",
+    "url": "./static/js/2.b53398e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2b2e77eebff26e1c56",
+    "url": "./static/js/main.8be56ac4.chunk.js"
+  },
+  {
+    "revision": "d0bbc77f793b850ef345",
+    "url": "./static/js/runtime-main.d8b35a80.js"
+  },
+  {
+    "revision": "a117d30c86990b5001eea70d94ccfe7d",
+    "url": "./static/media/photo1.a117d30c.jpg"
+  }
+]);
